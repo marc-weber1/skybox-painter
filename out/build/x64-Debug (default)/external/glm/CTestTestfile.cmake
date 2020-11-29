48f348b5ea6ee9e75bced3a49a06f2850c1771e5
Jashanjot Dhilon <jashanjotdhilon@gmail.com>
2020-11-28 18:05:54 -0700

    Setup GUI skeleton
    Added Import, Export dummy buttons
    Added sample brush selection
    Added functional colour selection

---
+++ out/build/x64-Debug (default)/external/glm/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/CPSC591/skybox-painter/external/glm
+# Build directory: D:/CPSC591/skybox-painter/out/build/x64-Debug (default)/external/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("glm")
